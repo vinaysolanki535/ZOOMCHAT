867ec38aed034fc32cc259204d3364810aadde29
vinaysolanki535 <47470634+vinaysolanki535@users.noreply.github.com>
2021-04-30 15:27:48 +0530

    complete psuh

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Typography, AppBar } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
 
-function App() {
+import VideoPlayer from './components/VideoPlayer'
+import Sidebar from './components/Sidebar'
+import Notifications from './components/Notifications'
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 100px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '600px',
+    border: '2px solid black',
+
+    [theme.breakpoints.down('xs')]: {
+      width: '90%',
+    },
+  },
+  image: {
+    marginLeft: '15px',
+  },
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+  },
+}))
+
+const App = () => {
+  const classes = useStyles()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.wrapper}>
+      <AppBar className={classes.appBar} position='static' color='inherit'>
+        <Typography variant='h2' align='center'>
+          ZOOM CHAT
+        </Typography>
+      </AppBar>
+      <VideoPlayer />
+      <Sidebar>
+        <Notifications />
+      </Sidebar>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDGrlP8phr3v1hXoOYU3JFGNmrdvHPyFFk',
+  authDomain: 'videochatapp-1d123.firebaseapp.com',
+  projectId: 'videochatapp-1d123',
+  storageBucket: 'videochatapp-1d123.appspot.com',
+  messagingSenderId: '986959155693',
+  appId: '1:986959155693:web:013a08941da4c1767357e4',
+}
